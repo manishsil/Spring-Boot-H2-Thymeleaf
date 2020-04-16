d83aebf18077c2002b6bcab4d1b2aa6fa19930a4
Manish <Manish@DESKTOP-1125Q6N>
2020-04-16 16:32:57 +0530

    Code to build as .war file
    Change-Id: I2fcbd78274cc417a9a0e19d022826f80f939f71a

--- springboot-thymeleaf-crud/src/main/java/com/manish/springbootthymeleafcrud/SpringbootThymeleafCrudApplication.java
+++ springboot-thymeleaf-crud/src/main/java/com/manish/springbootthymeleafcrud/SpringbootThymeleafCrudApplication.java
@@ -2,12 +2,19 @@ package com.manish.springbootthymeleafcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootThymeleafCrudApplication {
+public class SpringbootThymeleafCrudApplication/* extends SpringBootServletInitializer */{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafCrudApplication.class, args);
 	}
+
+	/*@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(SpringbootThymeleafCrudApplication.class);
+	 }*/
 
 }
